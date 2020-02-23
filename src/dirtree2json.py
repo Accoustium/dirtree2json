79ad1a344b7fbdff2e596c89d05e3606311d8698
Accoustium <chimchim8@hotmail.com>
2020-02-23 17:16:10 -0600

    fix recursive class __walk_tree

--- src/dirtree2json.py
+++ src/dirtree2json.py
@@ -10,7 +10,7 @@ class Tree:
     source: str
     curr_dir: str
 
-    def __init__(self, source: str=os.getcwd()):
+    def __init__(self, source: str = os.getcwd()):
         if type(source) == str:
             print(source)
             if os.path.isdir(source):
@@ -21,26 +21,22 @@ class Tree:
                 raise SyntaxError
         else:
             raise TypeError
-    
+
     def __repr__(self):
         return f"Tree(source={self.source})"
-    
+
     def __str__(self):
         return f"{self.tree}"
 
     def __walk_path(self, source_path: str) -> list:
         path_list = os.listdir(source_path)
         for index, file_dir in enumerate(path_list):
-            if os.path.isdir(file_dir):
+            if os.path.isdir(os.path.join(source_path, file_dir)):
                 path_list[index] = {
-                    file_dir: self.__walk_path(
-                        os.path.join(source_path, file_dir)
-                    )
+                    file_dir: self.__walk_path(os.path.join(source_path, file_dir))
                 }
-        
+
         return path_list
 
     def walk_tree(self):
-        self.tree.update({
-            'source': self.__walk_path(self.source)
-        })
+        self.tree.update({"source": self.__walk_path(self.source)})
