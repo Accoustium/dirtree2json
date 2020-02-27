5d947a9999da47517afcdab5ce4475f984ed1340
Accoustium <chimchim8@hotmail.com>
2020-02-26 23:24:37 -0600

    make it so you can specify the depth

--- src/dirtree2json.py
+++ src/dirtree2json.py
@@ -6,13 +6,14 @@ from dataclasses import dataclass
 class Tree:
     tree: dict
     source: str
-    curr_dir: str
+    depth: int
 
-    def __init__(self, source: str = os.getcwd()):
+    def __init__(self, source: str = os.getcwd(), depth: int=1):
         if type(source) == str:
             if os.path.isdir(source):
                 self.source = source
                 self.tree = {}
+                self.depth = depth
                 self.walk_tree()
             else:
                 raise SyntaxError
@@ -25,15 +26,26 @@ class Tree:
     def __str__(self):
         return f"{self.tree}"
 
-    def __walk_path(self, source_path: str) -> list:
-        path_list = os.listdir(source_path)
+    def __walk_path(self, source_path: str, length: int) -> list:
+        length += 1
+        try:
+            path_list = os.listdir(source_path)
+        except PermissionError:
+            return ['Permission Denied.  Please verify permissions.']
         for index, file_dir in enumerate(path_list):
             if os.path.isdir(os.path.join(source_path, file_dir)):
-                path_list[index] = {
-                    file_dir: self.__walk_path(os.path.join(source_path, file_dir))
-                }
+                if length != self.depth:
+                    path_list[index] = {
+                        file_dir: [
+                            self.__walk_path(os.path.join(source_path, file_dir), length + 1)
+                        ]
+                    }
+                else:
+                    path_list[index] = {
+                        file_dir: []
+                    }
 
         return path_list
 
     def walk_tree(self):
-        self.tree.update({"source": self.__walk_path(self.source)})
+        self.tree.update({"source": self.__walk_path(self.source, length=0)})
