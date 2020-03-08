a78f24989db9bf96eefb79273e7af885d7b894b2
Accoustium <chimchim8@hotmail.com>
2020-03-08 12:51:36 -0500

    update builds and tests

--- test/test_dirtree.py
+++ test/test_dirtree.py
@@ -1,4 +1,16 @@
+import os
 import sys
 import pytest
 
+mypath = os.path.join(os.path.split(__file__)[:-1][0])
+sys.path.append(os.path.join(mypath, '..'))
 from src import Tree
+
+
+@pytest.fixture
+def tree_at_source():
+    return Tree()
+
+
+def test_tree_instance(tree_at_source):
+    assert isinstance(tree_at_source, Tree)
