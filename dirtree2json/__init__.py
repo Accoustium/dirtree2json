799d643d3b9ac0766760a3d2b623573eb858adc3
Accoustium <chimchim8@hotmail.com>
2020-12-04 15:53:37 -0600

    Remove __version and __version_info__ from __init__ file import

--- dirtree2json/__init__.py
+++ dirtree2json/__init__.py
@@ -1,4 +1,3 @@
 from .dirtree2json import Tree
 from .filetype import FileTypeError, File, Directory
-from ._version import __version__, __version_info__
 __all__ = ['Tree', 'FileTypeError', 'File', 'Directory']
