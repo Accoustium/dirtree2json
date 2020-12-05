e5fb71bafcc99a82851be1f75403f7c2ab0bbedd
Accoustium <chimchim8@hotmail.com>
2020-12-05 16:53:05 -0600

    Add more testing.  Getting ready for more tests.

--- test/test_dirtree.py
+++ test/test_dirtree.py
@@ -7,10 +7,14 @@ sys.path.append(os.path.join(file_path, ".."))
 from dirtree2json import Tree, File, Directory, FileTypeError
 
 
-@pytest.fixture
+@pytest.fixture(scope="module")
 def tree_at_source():
     return Tree()
 
 
 def test_tree_instance(tree_at_source):
     assert isinstance(tree_at_source, Tree)
+
+
+def test_print_tree(tree_at_source):
+    print(tree_at_source.display)
